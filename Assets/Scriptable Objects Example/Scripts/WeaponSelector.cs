20416a54eaa4e5a8eb34ec0aa4cdf61a30d12d96
Indika Wijesooriya <indikman@gmail.com>
2024-07-13 11:52:39 -0600

    End of july 13 class

--- Assets/Scriptable Objects Example/Scripts/WeaponSelector.cs
+++ Assets/Scriptable Objects Example/Scripts/WeaponSelector.cs
@@ -6,14 +6,38 @@ using UnityEngine;
 public class WeaponSelector : MonoBehaviour
 {
     public event Action<WeaponDataSO> OnWeaponLoaded;
+    public event Action<WeaponDataSO> OnWeaponSelected;
+    public event Action<WeaponDataSO> OnWeaponChosen;
     
     [SerializeField] private WeaponDataSO[] avaialbleWeapons;
 
+    private WeaponDataSO _selectedWeapon;
+    private WeaponDataSO _chosenWeapon;
+    
     private void LoadWeapons()
     {
         foreach (var weaponData in avaialbleWeapons)
         {
             OnWeaponLoaded?.Invoke(weaponData);
+        }
+    }
+
+    /// <summary>
+    /// This will be executed by the weapon button presses
+    /// </summary>
+    /// <param name="weaponData"></param>
+    public void SelectWeapon(WeaponDataSO weaponData)
+    {
+        _selectedWeapon = weaponData;
+        OnWeaponSelected?.Invoke(_selectedWeapon);
+    }
+
+    public void ChooseWeapon()
+    {
+        if (_selectedWeapon)
+        {
+            _chosenWeapon = _selectedWeapon;
+            OnWeaponChosen?.Invoke(_chosenWeapon);
         }
     }
     
--- Assets/Scriptable Objects Example/Scripts/WeaponSelectorUI.cs
+++ Assets/Scriptable Objects Example/Scripts/WeaponSelectorUI.cs
@@ -1,29 +1,74 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponSelectorUI : MonoBehaviour
 {
     [SerializeField] private WeaponSelector weaponSelector;
 
+    [SerializeField] private GameObject weaponUI;
+    
     [Header("Weapon Button Panel")] 
     [SerializeField] private WeaponButton weaponButtonPrefab;
     [SerializeField] private Transform weaponButtonPanel;
 
+    [Header("Weapon Info Display")] 
+    [SerializeField] private TMP_Text txtName;
+    [SerializeField] private TMP_Text txtDescription;
+    [SerializeField] private TMP_Text txtDamage;
+    [SerializeField] private TMP_Text txtStrength;
+    [SerializeField] private TMP_Text txtReload;
+    [SerializeField] private Image imgWeapon;
+    
     private void OnEnable()
     {
         weaponSelector.OnWeaponLoaded += PopulateWeaponButton;
+        weaponSelector.OnWeaponSelected += PopulateWeaponSelection;
     }
 
     private void OnDisable()
     {
         weaponSelector.OnWeaponLoaded -= PopulateWeaponButton;
+        weaponSelector.OnWeaponSelected -= PopulateWeaponSelection;
     }
 
     private void PopulateWeaponButton(WeaponDataSO weaponData)
     {
         var newButton = Instantiate(weaponButtonPrefab, weaponButtonPanel);
         newButton.SetButton(weaponData);
+        
+        // Set the event listener to the button to select the weapon
+        var button = newButton.GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            Debug.Log("Button Pressed!");
+            weaponSelector.SelectWeapon(weaponData);
+        } );
+    }
+
+    private void PopulateWeaponSelection(WeaponDataSO weaponData)
+    {
+        txtName.text = weaponData.weaponName;
+        txtDescription.text = weaponData.weaponDescription;
+        txtDamage.text = $"Damage: {weaponData.damage}";
+        txtStrength.text = $"Strength: {weaponData.strength}";
+        txtReload.text = $"Reload Speed: {weaponData.reloadSpeed}";
+        imgWeapon.sprite = weaponData.sprite;
+    }
+
+    public void ToggleWeaponUI()
+    {
+        CleanWeaponInfo();
+        weaponUI.SetActive(!weaponUI.activeSelf);
+    }
+
+    private void CleanWeaponInfo()
+    {
+        txtName.text = "Select Weapon";
+        txtDescription.text = "";
+        txtDamage.text = "";
+        txtStrength.text = "";
+        txtReload.text = "";
+        imgWeapon.sprite = null;
     }
 }
